624025315521137cb089ba167a892d617c948ea7
riaqliu <riaqliu@gmail.com>
2025-06-19 00:21:33 +0800

    Added new button types

--- electron-vite-vue/src/components/generic/Button.vue
+++ electron-vite-vue/src/components/generic/Button.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 const props = defineProps<{
-    delete: boolean,
-    expand: boolean
+    delete?: boolean,
+    expand?: boolean,
+    plus?: boolean
 }>()
 
 const emit = defineEmits<{
@@ -13,12 +14,13 @@ const emit = defineEmits<{
 <button @click="emit('click')">
     <img v-if="delete" src="@/assets/icon/delete-white.svg" alt="Delete"/>
     <img v-else-if="expand" src="@/assets/icon/expand-black.svg" alt="Expand">
+    <img v-else-if="plus" src="@/assets/icon/plus-black.svg" alt="Expand">
 </button>
 </template>
 <style lang="scss" scoped>
-img {
-    position: relative;
-    width: 100%;
-    height: 100%;
-}
+    img {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
 </style>
