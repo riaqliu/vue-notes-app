e93f9a099df19d7c7c0ab569174f6fc73017ea7b
riaqliu <riaqliu@gmail.com>
2025-06-24 00:29:08 +0800

    Add testPhrases constants file

---
+++ electron-vite-vue/src/constants/testPhrases.ts
@@ -0,0 +1,22 @@
+export const testPhrases: string[] = [
+  "The quick brown fox jumps over the lazy dog",
+  "Hello world!",
+  "TypeScript is awesome",
+  "Keep it simple, stupid",
+  "Lorem ipsum dolor sit amet",
+  "Test-driven development rocks",
+  "Edge cases matter",
+  "I love clean code",
+  "This is a placeholder",
+  "Async and await forever",
+  "Debugging is like being a detective",
+  "Another day, another bug",
+  "Refactor mercilessly",
+  "Don't repeat yourself",
+  "Null is not a value",
+  "Use meaningful names",
+  "Ship it!",
+  "Works on my machine",
+  "One more test won’t hurt",
+  "Comment your code wisely"
+];
