ce00cc2db9c92434723724c47811c78477d733e0
riaqliu <riaqliu@gmail.com>
2025-06-24 10:56:56 +0800

    Add dropdown button implementation

---
+++ electron-vite-vue/src/components/__generics__/DropdownButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isDropdownOpen = ref(false);
+
+</script>
+<template>
+    <div class="dropdown-button">
+        <Button @click="isDropdownOpen=!isDropdownOpen"/>
+        <template v-if="isDropdownOpen">
+            <div class="dropdown-menu">
+                <slot></slot>
+            </div>
+        </template>
+    </div>
+</template>
+<style scoped lang="scss">
+@use "@/stylesheets/default.scss" as *;
+
+.dropdown-button {
+    position: relative;
+    display: inline-block;
+}
+
+.dropdown-menu {
+    padding: 0;
+    margin: 0;
+    display: inline-block;
+    position: absolute;
+}
+</style>
